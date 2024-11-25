efe227fa1714edffce0c61c958701c33e94ce8ae
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-11-26 00:11:51 +0600

    deleted functionality added

--- simple-crud-client/src/Users.jsx
+++ simple-crud-client/src/Users.jsx
@@ -3,6 +3,20 @@ import { useLoaderData } from "react-router-dom";
 export default function Users() {
   const users = useLoaderData();
 
+  const handleDelete = (id) => {
+    // console.log(id);
+    fetch(`http://localhost:3000/users/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deleteCount > 0) {
+          alert("deleted successfully");
+        }
+      });
+  };
+
   return (
     <div>
       <h2>Users {users.length}</h2>
@@ -12,6 +26,7 @@ export default function Users() {
             {user.name}
             {" : "}
             {user.email}
+            {" : "} <button onClick={() => handleDelete(user._id)}>X</button>
           </div>
         ))}
       </div>
--- simple-crud-server/index.js
+++ simple-crud-server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
 const app = express();
 
@@ -43,6 +43,15 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
