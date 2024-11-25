032c11d44e85dc13fb6e75efc75d6a7fc87190fa
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-11-25 13:02:25 +0600

    creat user and sent to db

--- simple-crud-client/src/App.jsx
+++ simple-crud-client/src/App.jsx
@@ -1,9 +1,40 @@
 import "./App.css";
 
 function App() {
+
+  const handleAddUser=(e)=>{
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+    const name = form.get('name');
+    const email = form.get('email');
+
+    const user = {name, email};
+
+    console.log(user)
+    fetch('http://localhost:3000/users',{
+      method:"POST",
+      headers:{
+        'Content-Type':'application/json',
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    
+
+  }
+
   return (
     <>
       <h1>Simple CRUD</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" />
+        <br />
+        <input type="email" name="email" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
     </>
   );
 }
--- simple-crud-server/index.js
+++ simple-crud-server/index.js
@@ -26,6 +26,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("userDB");
+    const userCollection = database.collection("userCollection");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log("New User", user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
